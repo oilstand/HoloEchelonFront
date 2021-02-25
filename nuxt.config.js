ff1cc9fa45e4d93d335d18487e2db56aa25f7b96
gcp <oilstand@shimesaba.jp>
2021-02-25 13:35:39 +0000

    OGP設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,8 +18,18 @@ export default {
     titleTemplate: '%s | HoloEchelon ホロライブスケジュール',
     title: 'title',
     meta: [
-      { charset: 'utf-8' },
-      { 'http-equiv': 'content-language', content:'ja' },
+        { charset: 'utf-8' },
+        { 'http-equiv': 'content-language', content:'ja' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: `ホロライブの配信スケジュールを確認できます` },
+        { hid: 'keywords', name: 'keywords', content: 'ホロライブ,配信スケジュール,VTuber' },
+        { hid: 'og:site_name', property: 'og:site_name', content: 'HoloEchelon ホロライブスケジュールサイト' },
+        { hid: 'og:type', property: 'og:type', content: 'website' },
+        { hid: 'og:url', property: 'og:url', content: 'https://holoechelon.com' },
+        { hid: 'og:title', property: 'og:title', content: `HoloEchelon ホロライブスケジュールサイト` },
+        { hid: 'og:description', property: 'og:description', content: 'ホロライブの配信スケジュールを確認できます' },
+        { hid: 'og:image', property: 'og:image', content: 'https://holoechelon.com/ogp_small.png' },
+        { name: 'twitter:card', content: 'summary' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
